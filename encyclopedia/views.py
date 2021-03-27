b24261409b87727b14a27dbdfea7b192a2133b70
Rafael <rafargc@gmail.com>
2021-03-27 20:53:36 -0300

    details done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import markdown2
+from django import forms
+
+from django.http import HttpResponse
+from django.urls import reverse
 
 from . import util
 
@@ -8,3 +13,18 @@ def index(request):
         "entries": util.list_entries()
     })
 
+class SearchForm(forms.Form): # lhs search form
+    search = forms.CharField(label='', 
+    widget=forms.TextInput(attrs={'placeholder':'Search Encyclopedia'}))
+
+
+def details(request, title):
+    if util.get_entry(title) is None:
+        return render(request, "encyclopedia/404.html", {
+            "message": "Requested page was not found"})
+    else:
+        return render(request, "encyclopedia/details.html", {
+            "title":title.capitalize(),
+            "entry":markdown2.markdown(util.get_entry(title)),
+            "form": SearchForm()
+        })
